3b6bc83bf363ab7d28453b6d7a07c7d85d3e5928
Divya Rani <ranidivya063@gmail.com>
2020-02-19 14:29:57 +0530

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://divya063-photo-app.herokuapp.com/', :protocol => 'https'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'https://divya063-photo-app.herokuapp.com', :protocol => 'https'}
 
+
+  ActionMailer::Base.smtp_settings = {
+    :from                 => 'ranidivya063@email.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :domain               => 'https://divya063-photo-app.herokuapp.com',
+    :address              => 'smtp.sendgrid.net',
+    :port                 => 587,
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
